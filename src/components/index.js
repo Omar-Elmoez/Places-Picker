69bdca76cd6d0610ef5f9ddc62c15bace03dd92b
Omar Elmoez <omarelmoez2000@gmail.com>
2023-12-14 08:13:39 +0200

    move progressbar to a seperate component to optimize state updates

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import DeleteConfirmation from "./DeleteConfirmation";
 import Modal from "./Modal";
 import Places from "./Places";
+import ProgressBar from "./ProgressBar";
 
-export { DeleteConfirmation, Modal, Places }
+export { DeleteConfirmation, Modal, Places, ProgressBar }
